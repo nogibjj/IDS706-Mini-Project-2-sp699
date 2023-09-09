b3c9b9292fa78159d54c972643b4fb3791df206f
suim.park <suim.park@duke.edu>
2023-09-09 02:41:19 -0400

    Add code using pandas

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-"""
-Main cli or app entry point
-"""
+# Main.py using pandas
 
-from mylib.calculator import add
-import click
-
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+import pandas as pd
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def GDP(data):
+    GDP_df = pd.DataFrame(data)
+    return GDP_df
--- test_main.py
+++ test_main.py
@@ -1,10 +1,20 @@
-"""
-Test goes here
+# Test main.py
 
-"""
-
-from mylib.calculator import add
+from main import GDP
 
 
-def test_add():
-    assert add(1, 2) == 3
+def test_GDP():
+    GDP_data = {
+        "year": [2021, 2022, 2023],
+        "GDP rate": [2.8, 3.1, 3.0],
+        "GDP": ["1.637M", "1.73M", "1.83M"],
+    }
+
+    result = GDP(GDP_data)
+
+    max_value = max(result["GDP rate"])
+    assert max_value == 3.1
+
+
+if __name__ == "__main__":
+    test_GDP()
